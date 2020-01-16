7fff86463a3d9990bd849279ba6f20c4e3050464
Gil Alves <gg.alves@campus.fct.unl.pt>
2020-01-16 10:19:18 +0000

    Lesson 154: Getting started with Socket.io.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -9,12 +13,14 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 // Public directory setup
 app.use(express.static(publicDirectoryPath));
 
-
-app.get('/', (req, res) => {
-    res.render('index');
+io.on('connection', () => {
+    console.log('New WebSocket connection.');
 });
 
 
-app.listen(port, () => {
+server.listen(port, () => {
    console.log(`Server is up and running on port ${port}.`);
 });
+
+
+/* The WebSocket Protocol */
