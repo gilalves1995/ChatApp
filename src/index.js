27df2fb442f6ab195d43a779aea023578a9118a2
Gil Alves <gg.alves@campus.fct.unl.pt>
2020-01-20 11:57:31 +0000

    Lesson 172: Rendering User List.

--- src/index.js
+++ src/index.js
@@ -32,6 +32,10 @@ io.on('connection', (socket) => {
 
         // Emits the message to every connection except this particular socket
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined.`));
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        });
         callback();
     });
 
@@ -60,6 +64,10 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id);
         if (user) {
             io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left the room.`));
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            });
         }
     });
 });
